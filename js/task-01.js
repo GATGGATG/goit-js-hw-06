101652fb3a922dfcdd6c0d6d5f613fd9827e63a4
gatggatg <guitogo@gmail.com>
2023-03-20 22:26:09 -0500

    first commit

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+
+const categoriesLi = document.querySelectorAll(`.item`)
+console.log(`Numbers of categories: ${categoriesLi.length}`);
+for(let element of categoriesLi){
+    console.log(`Category: ${element.querySelector(`h2`).textContent}`)
+    console.log(`Elements: ${element.querySelectorAll(`li`).length}`)
+}
---
+++ js/task-10.js
@@ -0,0 +1,42 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+const controls = {
+numbersEl: document.querySelector(`#controls`).children [0],
+createButton: document.querySelector(`#controls`).children [1],
+destroyButton: document.querySelector(`#controls`).children [2],
+}  
+const boxes = document.querySelector(`#boxes`)
+
+controls.createButton.addEventListener(`click`, () => {
+createBoxes(controls.numbersEl.value)
+
+});
+
+controls.destroyButton.addEventListener(`click`, () => {
+  createBoxes.innerHTML = ``
+  
+  })
+
+  function createBoxes (amount) {
+    for (let i = 1; i<= amount; i += 1)  
+    {
+      const newBox = document.createElement(`div`);
+      const divIdName = i ;
+
+      newBox.style.width = 20 + i*10 + `px`;
+      newBox.style.height = 20 + i*10 + `px`;
+
+      newBox.setAttribute(`id`,divIdName);
+      newBox.style.backgroundColor = getRandomHexColor();
+
+      controls.numbersEl.value = ``;
+
+      boxes.append(newBox);
+      
+    }
+  }
